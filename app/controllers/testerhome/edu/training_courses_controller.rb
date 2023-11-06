bc4b345ed3f1328b90198bf57550a860d0a47a08
zhanglihua <lihuazhang@hotmail.com>
2023-11-06 23:22:52 +0800

    add config to two pages

--- app/controllers/testerhome/edu/training_courses_controller.rb
+++ app/controllers/testerhome/edu/training_courses_controller.rb
@@ -8,6 +8,16 @@ module Testerhome::Edu
       @training_courses = training_courses_scope.order('created_at desc, id desc').page(params[:page]).per(10)
     end
 
+    def teacher_cooperate
+      training_courses_scope = TrainingCourse.all.published
+      @suggested_courses = training_courses_scope.suggested.limit(10)
+    end
+
+    def business_cooperate
+      training_courses_scope = TrainingCourse.all.published
+      @suggested_courses = training_courses_scope.suggested.limit(10)
+    end
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_training_course
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Testerhome::Edu::Engine.routes.draw do
   resources :training_courses
+  get :teacher_cooperate, to: "training_courses#teacher_cooperate", as: "teacher_cooperate"
+  get :business_cooperate, to: "training_courses#business_cooperate", as: "business_cooperate"
   namespace :admin do
     resources :training_courses do
       member do
